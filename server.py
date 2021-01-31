cd0adda7dc81bc1442efcb6e07b70336b259a14b
BLACK CAT <BLACK-CAT13@users.noreply.github.com>
2021-01-31 06:22:49 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+import tqdm
+import os
+
+
+
+
+# device's IP address
+SERVER_HOST =  socket.gethostbyname(socket.gethostname())
+SERVER_PORT = 12345
+
+BUFFER_SIZE = 4096 # receive 4096 bytes each time
+SEPARATOR = "<SEPARATOR>"
+
+
+# create the server socket
+s = socket.socket()
+# bind the socket to our local address
+s.bind((SERVER_HOST, SERVER_PORT))
+# enabling our server to accept connections
+# 5 here is the number of unaccepted connections that the system will allow before refusing new connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+# accept connection if there is any
+client_socket, address = s.accept() 
+# when the client connect print...:
+print(f"[+] {address} is connected.")
+
+
+# receive the file information 
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+# remove absolute path if there is
+filename = os.path.basename(filename)
+# convert to integer
+filesize = int(filesize)
+
+# start receiving the file from the socket
+# and writing to the file stream
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "wb") as f:
+    for _ in progress:
+        # read 1024 bytes from the socket (receive)
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:    
+            # nothing is received
+            # file transmitting is done
+            break
+        # write to the file the bytes we just received
+        f.write(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+
+# close the client socket
+client_socket.close()
+# close the server socket
+s.close()
